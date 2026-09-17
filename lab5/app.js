8fa2bac43fc69900a3e4968de7c05c5ef4bad23f
arpitpatelvns19-ux <arpitpatelvns19@gmail.com>
2026-09-17 13:59:23 +0530

    go

--- lab5/app.js
+++ lab5/app.js
@@ -1,10 +1,31 @@
-import express from 'express';
+import express from 'express'
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send('Hello Express');
+    res.send("<h1> Hello Express </h1>");
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+app.get('/about', (req, res) => {
+    res.end("We are Full Stack Developer")
 });
+
+app.post('/login', (req, res) => {
+    res.send({msg: 'user login'})
+});
+
+app.put('/user/update/1', (req, res) => {
+    res.send({msg: 'user update'})
+});
+
+app.delete('/users/1', (req, res) => {
+    res.send({msg: 'removie user 1'})
+});
+
+app.use((req, res) => {
+    res.status(404).send("Not Found")
+});
+
+
+
+app.listen(3333, () => console.log("Server is running "));
